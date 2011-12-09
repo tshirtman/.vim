1435b634af250c86a6da419bbd5c7863c60283c0
gabriel.pettier <gabriel.pettier@gmail.com>
2011-12-09 23:44:49 +0100

    added readme, and updated init.sh,

---
+++ README.md
@@ -0,0 +1,12 @@
+tshirtman vim config
+===
+
+run ./init after cloning
+
+it will try to link ~/.vimrc to the vimrc file here, wich will fail if you
+already have a .vimrc, in which case, do the merge yourself
+
+it should update submodules
+
+it should install required packages if you are on a debian based system (will
+ask your password if needed)
--- init.sh
+++ init.sh
@@ -1,5 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 # run this script after cloning repo
 
 git submodule init
 git submodule update
+
+while true
+do
+read -p "do you want to link vimrc to ~/.vimrc (Y/N)" yn
+case $yn in
+	[Yy]*) ln -s vimrc ~/.vimrc; break;;
+	[Nn]*) break;;
+	*) echo "please answer yes or no";;
+esac
+done
+
+while true
+do
+read -p "do you want to install packages? (Y/N)" yn
+case $yn in
+	[Yy]*) sudo apt-get install vim-nox;break;;
+	[Nn]*) break;;
+	*) echo "please answer yes or no";;
+esac
+done
+

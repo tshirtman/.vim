95d1b7cd29d2258b0fb34fb8e2b16b7edf4050c0
gabriel.pettier <gabriel.pettier@gmail.com>
2011-12-09 23:52:27 +0100

    correct .vimrc link creation

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ while true
 do
 read -p "do you want to link vimrc to ~/.vimrc (Y/N)" yn
 case $yn in
-	[Yy]*) ln -s vimrc ~/.vimrc; break;;
+	[Yy]*) ln -s ~/.vim/vimrc ~/.vimrc; break;;
 	[Nn]*) break;;
 	*) echo "please answer yes or no";;
 esac

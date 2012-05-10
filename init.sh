aa848ad9e309799beb0c9a1e66b42940dc94a7bf
gabriel.pettier <gabriel.pettier@gmail.com>
2012-05-10 20:02:28 +0200

    fix packages for ruby

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ while true
 do
 read -p "do you want to install packages? (Y/N)" yn
 case $yn in
-	[Yy]*) sudo apt-get install vim-nox python-simplejson exuberant-ctags libxslt1-dev libxml2-dev; break;;
+	[Yy]*) sudo apt-get install vim-nox python-simplejson exuberant-ctags libxslt1-dev libxml2-dev ruby-bundler rubygems rake; break;;
 	[Nn]*) break;;
 	*) echo "please answer yes or no";;
 esac

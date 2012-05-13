0043c07233493e1702115530f3c262d8957490b2
gabriel.pettier <gabriel.pettier@gmail.com>
2012-05-13 15:13:05 +0200

    remove command-t, ctrlP is great and has less dependancies

--- init.sh
+++ init.sh
@@ -18,15 +18,8 @@ while true
 do
 read -p "do you want to install packages? (Y/N)" yn
 case $yn in
-	[Yy]*) sudo apt-get install vim-nox python-simplejson exuberant-ctags libxslt1-dev libxml2-dev ruby-bundler rubygems rake; break;;
+	[Yy]*) sudo apt-get install vim-nox python-simplejson exuberant-ctags; break;;
 	[Nn]*) break;;
 	*) echo "please answer yes or no";;
 esac
 done
-
-# build command-t
-pushd bundle/command_t/ruby/command-t/
-bundle install
-rake make
-popd
-

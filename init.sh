27a3429b954bb4677e97fde6290795099f24eb48
gabriel.pettier <gabriel.pettier@gmail.com>
2012-06-27 23:53:35 +0200

    add curl installation in init.sh

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ while true
 do
 read -p "do you want to install packages? (Y/N)" yn
 case $yn in
-	[Yy]*) sudo apt-get install vim-nox python-simplejson exuberant-ctags; break;;
+	[Yy]*) sudo apt-get install vim-nox python-simplejson exuberant-ctags curl; break;;
 	[Nn]*) break;;
 	*) echo "please answer yes or no";;
 esac

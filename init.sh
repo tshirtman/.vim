e3fe9816e572a1862c3311eadbc805ed6f119325
tshirtman <gabriel.pettier@gmail.com>
2012-11-19 03:07:02 +0100

    add install of flake8 (from pip) in init.sh

--- init.sh
+++ init.sh
@@ -20,7 +20,9 @@ while true
 do
 read -p "do you want to install packages? (Y/N)" yn
 case $yn in
-	[Yy]*) sudo apt-get install vim-nox python-simplejson exuberant-ctags curl ack-grep; break;;
+	[Yy]*) sudo apt-get install vim-nox python-simplejson exuberant-ctags curl ack-grep;
+		sudo pip install flake8
+		break;;
 	[Nn]*) break;;
 	*) echo "please answer yes or no";;
 esac
